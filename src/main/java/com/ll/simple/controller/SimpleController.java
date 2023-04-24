f2b92f3c51a8cf78c004f880806dd586df377b37
elephantLikesApple <elephant890@naver.com>
2023-04-24 13:35:48 +0900

    init

---
+++ src/main/java/com/ll/simple/controller/SimpleController.java
@@ -0,0 +1,13 @@
+package com.ll.simple.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SimpleController {
+    @GetMapping
+    public String main() {
+        return "index";
+    }
+}
